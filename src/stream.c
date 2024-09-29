934485194c114a981cb34d9dd819ad651436c0ce
incog <inito.inbox@gmail.com>
2024-09-30 05:01:21 +1000

    stream: loading from file

--- src/stream.c
+++ src/stream.c
@@ -1,4 +1,5 @@
 #include "stream.h"
+#include "list.h"
 
 #include <string.h>
 #include <stdlib.h>
@@ -11,7 +12,45 @@ stream_t stream_init(void) {
     ret.size = 0;
     ret.capacity = INITIAL_CAPACITY;
     ret.data = (char*) malloc(sizeof(char) * ret.capacity);
+    ret.it = ret.data;
     return ret;
+}
+
+void stream_free(stream_t* stream) {
+    stream->size = 0;
+    stream->capacity = INITIAL_CAPACITY;
+    free(stream->data);
+    stream->data = NULL;
+    stream->it = stream->data;
+}
+
+int stream_save_to_file(stream_t* stream, const char* path) {
+    FILE* file = fopen(path, "wb");
+    if (file == NULL) return 0;
+
+    fwrite(stream->data, sizeof(char), stream->size, file);
+
+    fclose(file);
+    return 1;
+}
+
+int stream_read_from_file(stream_t* stream, const char* path) {
+    FILE* file = fopen(path, "rb");
+    if (file == NULL) return 0;
+
+    fseek(file, 0, SEEK_END);
+    size_t size = ftell(file);
+    rewind(file);
+
+    free(stream->data);
+    stream->size = 0;
+    stream->capacity = size;
+    stream->data = (char*) malloc(sizeof(char) * stream->capacity);
+    stream->it = stream->data;
+
+    fread(stream->data, sizeof(char), size, file);
+    fclose(file);
+    return 1;
 }
 
 void stream_reserve(stream_t* stream, size_t size) {
@@ -50,3 +89,37 @@ void stream_push_category(stream_t* stream, const category_t* category) {
         stream_push_category(stream, list_get(category->sub_categories, i));
 }
 
+void* stream_pop(stream_t* stream, size_t size) {
+    if (stream->it - stream->data + size > stream->capacity) return NULL;
+    stream->it += size;
+    return stream->it - size;
+}
+
+void stream_pop_string(stream_t* stream, char* data) {
+    strcpy(data, stream->it);
+    stream->it += strlen(data) + 1;
+}
+
+account_t* stream_pop_account(stream_t* stream) {
+    char name[MAX_NAME_SIZE];
+    stream_pop_string(stream, name);
+    char password[MAX_PASSWORD_SIZE];
+    stream_pop_string(stream, password);
+    return account_init(name, password);
+}
+
+category_t* stream_pop_category(stream_t* stream) {
+    char name[MAX_NAME_SIZE];
+    stream_pop_string(stream, name);
+    category_t* category = category_init(name);
+
+    int i;
+    for (i = *(int*) stream_pop(stream, sizeof(int)); i > 0; --i) {
+        list_append(category->accounts, stream_pop_account(stream));
+    }
+
+    for (i = *(int*) stream_pop(stream, sizeof(int)); i > 0; --i)
+        list_append(category->sub_categories, stream_pop_category(stream));
+
+    return category;
+}
--- src/stream.h
+++ src/stream.h
@@ -6,15 +6,24 @@
 
 typedef struct {
    char* data;
+   char* it;
    size_t size, capacity;
 } stream_t;
 
 stream_t stream_init(void);
+void stream_free(stream_t*);
+
+int stream_save_to_file(stream_t*, const char*);
+int stream_read_from_file(stream_t*, const char*);
 
 void stream_push(stream_t*, const void*, size_t);
 void stream_push_string(stream_t*, const char*);
 void stream_push_account(stream_t*, const account_t*);
 void stream_push_category(stream_t*, const category_t*);
 
+void* stream_pop(stream_t*, size_t);
+void stream_pop_string(stream_t*, char*);
+account_t* stream_pop_account(stream_t*);
+category_t* stream_pop_category(stream_t*);
 
 #endif
